54a7d74cfd3bf42a68f95150449d11bf5c8b88d1
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-10-27 11:09:13 +0300

    Make intent launching safe

--- app/src/main/java/me/dmitvitalii/shiva/MainActivity.kt
+++ app/src/main/java/me/dmitvitalii/shiva/MainActivity.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-    val ACTIVITY  = 0
-    val SERVICE   = 1
-    val BROADCAST = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +17,17 @@ class MainActivity : AppCompatActivity() {
 
     fun onSendClicked(button: View) {
         val intent = Intent()
+        // TODO: implement an IntentBuilder instead
         with(intent) {
-            action = text_action.text.toString()
-            setPackage(text_package.text.toString())
-            addCategory(text_category.text.toString())
+            setAction(text_action)
+            setPackage(text_package)
+            setCategory(text_category)
         }
-        start(intent)
+        if (intent.getPackage().isNullOrEmpty()) {
+            Toast.makeText(this, "Package is required", Toast.LENGTH_SHORT).show()
+        } else {
+            start(intent)
+        }
     }
 
     fun start(intent: Intent) {
